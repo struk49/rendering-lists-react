4d93269d0ba2171a54034c2e7fbde3f06d56d440
Andrew Struk <andrewstruk@yahoo.co.uk>
2024-06-19 08:20:53 +0000

    renderinglists in react

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RenderingLists from './components/RenderingLists';
+
+function App() {
+  return (
+    <div className="App">
+     <RenderingLists />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/RenderingLists.js
@@ -0,0 +1,48 @@
+import React from 'react'
+
+function RenderingLists() {
+    const bookList = [
+        'To kill a Mockingbird',
+        'The Great Gatsby',
+        'The Catcher in the Rye',
+    ]
+
+    const books = [
+        {
+        title: 'To kill a Mockingbird',
+        author: 'Harper Lee',
+        pages: 281
+        },
+        {
+            title: 'The Great Gatsby',
+            author: 'F. Scott Fitzgerald',
+            pages: 218
+        },
+        {
+            title: 'The Catcher in the rye',
+            author: 'J. D. sallinger',
+            pages: 234
+        }
+
+    ]
+    return (
+        <div>
+            {bookList.map(book => {
+                return <h2>{book}</h2>
+            })}
+            <hr />
+            {books.map(book => {
+               return(
+                <div>
+                <h5>{book.title}</h5>
+                <p>{book.author}</p>
+                <p>{book.pages}</p>
+                </div>
+               )
+            }
+            )}
+        </div>
+    )
+}
+
+export default RenderingLists
